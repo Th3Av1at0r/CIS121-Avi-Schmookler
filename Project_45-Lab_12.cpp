947d8e3fce71f91dd5f4d58707c04093f52fb18d
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-11-17 16:34:36 -0600

    Add files via upload

---
+++ Project_45-Lab_12.cpp
@@ -0,0 +1,34 @@
+/*
+
+Output Commands
+Avi Schmookler
+11/10/2021
+
+*/
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <random>
+#include <vector>
+#include <Windows.h>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include ""
+
+
+int main()
+{
+    system("color 0a");
+    cout << "\tAvi Schmookler\n\tDate: 11/10/2021" << endl;
+    cout << setw(19) << "Lab#: N/A\n\t";
+    cout << "\"Project_45-Lab_12\"\n\tObjectives: In main( ) display all the defaults and then prompt the user to Deposit, Withdraw,  Balance or Quit."
+        << "Repeat until quit.Modify each private Datatype and re - Display\n\n";
+
+    return 0;
+}
